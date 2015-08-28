bf284d77aab7d3ff90d3a050af3580f9e1fd9e08
Rick <rh-jobs@outlook.com>
2015-08-28 14:11:44 -0700

    Add shell files and folders for routes and websites.

---
+++ app/app.php
@@ -0,0 +1,30 @@
+<?php
+    require_once __DIR__."/../vendor/autoload.php";
+    require_once __DIR__."/../src/Brand.php";
+    require_once __DIR__."/../src/Store.php";
+
+    $app = new Silex\Application();
+
+    $app['debug'] = true;
+
+    $server = 'mysql:host=localhost;dbname=shoes';
+    $username = 'root';
+    $password = '';
+    $DB = new PDO($server, $username, $password);
+
+
+    $app->register(new Silex\Provider\TwigServiceProvider(), array(
+        'twig.path' => __DIR__.'/../views'
+    ));
+
+    use Symfony\Component\HttpFoundation\Request;
+    Request::enableHttpMethodParameterOverride();
+
+
+    $app->get("/", function() use ($app) {
+        return $app['twig']->render('index.html.twig');
+    });
+
+    return $app; 
+
+?>

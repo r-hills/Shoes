44c6c2ad223978524d7a19756d0b65217c0e4adc
Rick <rh-jobs@outlook.com>
2015-08-28 17:08:11 -0700

    Files.

--- app/app.php
+++ app/app.php
@@ -25,6 +25,57 @@
         return $app['twig']->render('index.html.twig');
     });
 
+// All Stores list pages 
+
+    $app->get("/stores", function() use ($app) {
+        return $app['twig']->render('stores.html.twig',
+            array('stores' => Store::getAll() ) );
+    });
+
+    $app->post("/stores", function() use ($app) {
+        $store = new Store( 
+            preg_quote($_POST['name'], "'"),
+            preg_quote($_POST['address'], "'"),
+            preg_quote($_POST['phone'], "'")
+        );
+        $store->save();
+        return $app['twig']->render('stores.html.twig',
+            array('stores' => Store::getAll() ) );
+    });
+
+// Single Store pages 
+
+    $app->get("/store/{id}", function($id) use ($app) {
+        $store = Store::find($id);
+        $brands = $store->getBrands();
+        // echo "BRANDS >>> ";
+        // var_dump($brands);
+        return $app['twig']->render('store.html.twig',
+            array('store' => $store, 'brands' => $brands ) );
+    });
+
+    $app->post("/store/{id}", function($id) use ($app) {
+        // echo "STORE ID is: ".$id; 
+        $brand = new Brand( 
+            preg_quote($_POST['brand'], "'")
+        );
+        $brand->save();
+        $store = Store::find($id);
+        $brands = $store->getBrands(); 
+        echo ">>> BRANDS >>> ";
+        var_dump($brands);
+        return $app['twig']->render('store.html.twig',
+            array('store' => $store, 'brands' => $brands ) );
+
+    });
+
+
+
+
+
+
+
+
     return $app; 
 
 ?>
